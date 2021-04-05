acd9b15a44bc6417ec8274abdf5126bc6a560861
Anshul Saha <anshulsaha15@gmail.com>
2021-04-05 11:22:40 +0530

    socket

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const cors = require("cors");
 const mongoose = require('mongoose');
+const socketIO = require("socket.io")
 
 require('dotenv').config()
 
 const app = express();
+const port = process.env.PORT || 5000;
+
+const server = app.listen(port, err => {
+    console.log(`API listening on port ${port}.`);
+    if (err) throw err;
+});
+
+const io = socketIO(server, { cors: true, origins: '*:*' });
 
 const mongoURI = process.env.DB_URI
 
@@ -22,9 +31,20 @@ app.use(cors());
 
 app.use('/room', require('./routes/room'))
 
-const port = process.env.PORT || 5000;
+const users = {};
 
-app.listen(port, err => {
-    console.log(`API listening on port ${port}.`);
-    if (err) throw err;
-});
+io.on('connection', socket=>{
+    socket.on('new-user-joined', name =>{
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name)
+    })
+
+    socket.on('send', message=>{
+        socket.broadcast.emit('receive', {message: message, name: users[socket.id]})
+    })
+
+    socket.on('disconnect', name=>{
+        socket.broadcast.emit('left', {name: users[socket.id]})
+        delete users[socket.id];
+    })
+})
